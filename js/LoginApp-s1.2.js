c622e4890d2a0da07228e8c215f808463bf1c388
Antoine <bouquetant@gmail.com>
2016-11-25 11:27:14 +0100

    Question 3.4

--- js/LoginApp-s1.2.js
+++ js/LoginApp-s1.2.js
@@ -3,11 +3,17 @@ angular.module('loginApp', []);
 
 angular.module('loginApp').controller('loginCtrl', loginCrtFnt);
 
-loginCrtFnt.$inject=['$scope', '$log'];
+loginCrtFnt.$inject = ['$scope', '$log'];
 
-function loginCrtFnt($scope, $log){
-    $scope.logAuth = function(){
+function loginCrtFnt($scope, $log) {
+
+    $scope.logAuth = function () {
         $log.info('user login', $scope.user.login);
         $log.info('user pwd', $scope.user.pwd);
+    };
+
+    $scope.logAuthObject = function (user) {
+        $log.info('user login', user.login);
+        $log.info('user pwd', user.pwd);
     };
 }
